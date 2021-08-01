d8762270624b8392d2fc710d468d711638378cf8
shaippinho <shaienepardo@gmail.com>
2021-07-31 21:09:01 -0300

    MediatR , CQRS and FluentValidation

---
+++ SMTC.API.TaxaJuros/Application/Commands/TaxaJurosCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using SMTC.Core.MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SMTC.API.TaxaJuros.Application.Commands
+{
+    public class TaxaJurosCommandHandler : CommandHandler, IRequestHandler<TaxaJurosUpdateCommand, ValidationResult>
+    {
+        public async Task<ValidationResult> Handle(TaxaJurosUpdateCommand request, CancellationToken cancellationToken)
+        {
+            if (!request.IsValid()) return request.ValidationResult;
+
+            return request.ValidationResult;
+        }
+    }
+}
---
+++ SMTC.API.TaxaJuros/Application/Commands/TaxaJurosUpdateCommand.cs
@@ -0,0 +1,35 @@
+﻿using SMTC.Core.MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace SMTC.API.TaxaJuros.Application.Commands
+{
+    public class TaxaJurosUpdateCommand : Command
+    {
+        public double taxaJuros { get; set; }
+
+        public TaxaJurosUpdateCommand(double taxaJuros)
+        {
+            this.taxaJuros = taxaJuros;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new TaxaJurosUpdateValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+
+    public class TaxaJurosUpdateValidation : AbstractValidator<TaxaJurosUpdateCommand>
+    {
+        public TaxaJurosUpdateValidation()
+        {
+            RuleFor(c => c.taxaJuros)
+               .NotNull()
+               .WithMessage("Taxa de Juros não pode ser vazio");
+        }
+    }
+}
